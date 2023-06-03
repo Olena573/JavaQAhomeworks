c6f3be90e194006abf6e14343c144719aa079427
Olena573 <linboykomail@gmail.com>
2023-06-03 17:42:56 +0300

    everything new

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,76 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-        System.out.printf("Something outputed");
-        System.out.printf("Something for the branch 2");
+        byte q = 1;
+        byte m = 3;
+        short w = 30;
+        short g = 45;
+        int e = 123;
+        int c = 341;
+        long r = 145345;
+        long ac = 569;
+        // numeric
 
-        
+        float ayu = 45.134F;
+        float ert = 679.346F;
+        double b = 56.1;
+        double d = 34.9;
+        // numeric with a dot
+
+        boolean tly = true;
+        boolean eui = false;
+        boolean opf = true;
+        boolean vbn = false;
+        // boolean
+
+        char Vb = 's';
+        char Je = 'g';
+        //letteric
+
+        String ahm = "Something";
+        String tuv = "Some word";
+        // some String values. Weren't in the homework, but I added them.
+
+        // here I put a println commands for each value
+        System.out.println(q);
+        System.out.println(m);
+        System.out.println("These values are byte");
+
+        System.out.println(w);
+        System.out.println(g);
+        System.out.println("These values are short");
+
+        System.out.println(e);
+        System.out.println(c);
+        System.out.println("These values are int");
+
+        System.out.println(r);
+        System.out.println(ac);
+        System.out.println("These values are long");
+
+        System.out.println(ayu);
+        System.out.println(ert);
+        System.out.println("These values are float");
+
+        System.out.println(d);
+        System.out.println(b);
+        System.out.println("These values are double");
+
+        System.out.println(tly);
+        System.out.println(eui);
+        System.out.println(opf);
+        System.out.println(vbn);
+        System.out.println("These values are boolean");
+
+        System.out.println(Vb);
+        System.out.println(Je);
+        System.out.println("These values are char");
 
 
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+
     }
-}
+
+
+    }
